7b8569c71037c1987bd19f5e2bc55506fa54d7fa
alex. goat programer.pro footballer. <159536199+ALEKSANDREMG@users.noreply.github.com>
2026-01-11 15:21:01 +0400

    Add files via upload

---
+++ Day 84/HW/hw.js
@@ -0,0 +1,52 @@
+// 2
+let negativeNum = -15;
+console.log(Math.abs(negativeNum));
+
+// 3
+let num1 = -8;
+let num2 = 5;
+
+if (Math.abs(num1) > Math.abs(num2)) {
+  console.log(num1);
+} else {
+  console.log(num2);
+}
+
+// 4
+let student1 = 6.9;
+console.log(Math.floor(student1));
+
+// 5
+let student2 = 4.3;
+console.log(Math.floor(student2));
+
+// 6
+let student3 = 2.1;
+console.log(Math.ceil(student3));
+
+// 7
+let student4 = 7.01;
+console.log(Math.ceil(student4));
+
+// 8
+let student5 = 3.6;
+console.log(Math.round(student5));
+
+// 9
+let student6 = 4.4;
+console.log(Math.round(student6));
+
+// 11
+let base = 5;
+console.log(Math.pow(base, 2));
+
+// 12
+let student7 = 9.8;
+console.log(Math.trunc(student7));
+
+// 13
+console.log(Math.min(3, 7, 2, 9));
+
+// 14
+console.log(Math.max(3, 7, 2, 9));
+
---
+++ Day 88/CW/cw.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+  console.log("alexandre mgaloblishvili");
+}, 5000);
+
+setInterval(function() {
+  console.log("alexandre");
+}, 500);
